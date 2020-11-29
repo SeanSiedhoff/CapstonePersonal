7997201adc2849bd48d137a681c77e25ad323cda
Sean Siedhoff <ss388214@southeast.edu>
2020-11-29 12:56:24 -0600

    order manager now reads from database

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php 
+	include('functions.php');
+        if (!isLoggedIn()) {
+	$_SESSION['msg'] = "You must log in first";
+	header('location: login.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dance Lesson Tracking</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+	<div class="header">
+		<h2>Home Page</h2>
+	</div>
+	<div class="content">
+		<!-- notification message -->
+		<?php if (isset($_SESSION['success'])) : ?>
+			<div class="error success" >
+				<h3>
+					<?php 
+						echo $_SESSION['success']; 
+						unset($_SESSION['success']);
+					?>
+				</h3>
+			</div>
+		<?php endif ?>
+		<!-- logged in user information -->
+		<div class="profile_info">
+                        <i class="fas fa-user"></i>
+			<div>
+				<?php  if (isset($_SESSION['user'])) : ?>
+					<strong><?php echo $_SESSION['user']['username']; ?></strong>
+
+					<small>
+						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
+						<br>
+						<a href="index.php?logout='1'" style="color: red;">logout</a>
+					</small>
+
+				<?php endif ?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
