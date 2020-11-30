4e2810de616dfe546db3bc09bc78e004ebd677f5
SeanSiedhoff <59942383+SeanSiedhoff@users.noreply.github.com>
2020-11-29 23:15:06 -0600

    separating from main index file

--- view/login.php
+++ view/login.php
@@ -1,25 +1,25 @@
 <?php include('../model/functions.php') ?>
 <?php include('header.php'); ?>
-        <div class="header">
-            <h2>Dance Lesson Tracking</h2>
-        </div>
-        <form method="post" action="login.php">
+<div class="header">
+    <h2>Dance Lesson Tracking</h2>
+</div>
+<form method="post" action="login.php">
 
-            <?php echo display_error(); ?>
+    <?php echo display_error(); ?>
 
-            <div class="input-group">
-                <label>Username</label>
-                <input type="text" name="username" >
-            </div>
-            <div class="input-group">
-                <label>Password</label>
-                <input type="password" name="password">
-            </div>
-            <div class="input-group">
-                <button type="submit" class="btn" name="login_btn">Login</button>
-            </div>
-            <p>
-                Not yet a member? <a href="register.php">Sign up</a>
-            </p>
-        </form>
+    <div class="input-group">
+        <label>Username</label>
+        <input type="text" name="username" >
+    </div>
+    <div class="input-group">
+        <label>Password</label>
+        <input type="password" name="password">
+    </div>
+    <div class="input-group">
+        <button type="submit" class="btn" name="login_btn">Login</button>
+    </div>
+    <p>
+        <a href="register.php">Sign up</a>
+    </p>
+</form>
 <?php include('footer.php'); ?>
--- view/register.php
+++ view/register.php
@@ -1,31 +1,31 @@
 <?php include('../model/functions.php') ?>
 <?php include('header.php'); ?>
 <div class="header">
-	<h2>Register</h2>
+    <h2>Register</h2>
 </div>
 <form method="post" action="register.php">
     <?php echo display_error(); ?>
-	<div class="input-group">
-		<label>Username</label>
-                <input type="text" name="username" value="<?php echo htmlspecialchars($username); ?>">
-	</div>
-	<div class="input-group">
-		<label>Email</label>
-		<input type="email" name="email" value="<?php echo htmlspecialchars($email); ?>">
-	</div>
-	<div class="input-group">
-		<label>Password</label>
-		<input type="password" name="password_1">
-	</div>
-	<div class="input-group">
-		<label>Confirm password</label>
-		<input type="password" name="password_2">
-	</div>
-	<div class="input-group">
-		<button type="submit" class="btn" name="register_btn">Register</button>
-	</div>
-	<p>
-		Already a member? <a href="login.php">Sign in</a>
-	</p>
+    <div class="input-group">
+        <label>Username</label>
+        <input type="text" name="username" value="<?php echo htmlspecialchars($username); ?>">
+    </div>
+    <div class="input-group">
+        <label>Email</label>
+        <input type="email" name="email" value="<?php echo htmlspecialchars($email); ?>">
+    </div>
+    <div class="input-group">
+        <label>Password</label>
+        <input type="password" name="password_1">
+    </div>
+    <div class="input-group">
+        <label>Confirm password</label>
+        <input type="password" name="password_2">
+    </div>
+    <div class="input-group">
+        <button type="submit" class="btn" name="register_btn">Register</button>
+    </div>
+    <p>
+        <a href="login.php">Sign in</a>
+    </p>
 </form>
 <?php include('footer.php'); ?>
